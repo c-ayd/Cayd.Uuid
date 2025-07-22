650acf2378f2fd76470bf5342efd29722751c067
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-22 22:26:07 +0200

    add uuid v2
    UUID v2 is outside of the RFC 9562's scope. However, it is still added as obsolete to point this out

---
+++ src/Cayd.Uuid/Versions/Uuid.V2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cayd.Uuid
+{
+    public static partial class Uuid
+    {
+        [Obsolete("UUIDv2 is for DCE Security UUIDs. As such, the definition of these UUIDs is outside the scope of this specification.")]
+        public static class V2
+        {
+        }
+    }
+}
