f70bf27338e6394571a834502afbbeb0c254166b
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-22 22:20:57 +0200

    add base class and static values

---
+++ src/Cayd.Uuid/Uuid.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Cayd.Uuid
+{
+    public static partial class Uuid
+    {
+        public static readonly Guid Max = new Guid(0xFFFFFFFF, 0xFFFF, 0xFFFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF);
+        public static readonly Guid Empty = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
+}
