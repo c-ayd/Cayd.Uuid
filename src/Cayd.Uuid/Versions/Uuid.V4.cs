d7cb9cce3c5e040689609a212d05d9a7cef21e10
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-23 12:35:13 +0200

    add uuid v4 generator

---
+++ src/Cayd.Uuid/Versions/Uuid.V4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Cayd.Uuid
+{
+    public static partial class Uuid
+    {
+        public static class V4
+        {
+            public static Guid Generate()
+            {
+                var bytes = new byte[16];
+
+#if NETSTANDARD2_0
+                using (var rng = RandomNumberGenerator.Create())
+                {
+                    rng.GetBytes(bytes);
+                }
+#else
+                RandomNumberGenerator.Fill(bytes);
+#endif
+
+                // 'ver' bits
+                bytes[6] = (byte)(0x40 | (bytes[6] & 0x3F));
+
+                // 'var' bits
+                bytes[8] = (byte)(0x80 | (bytes[8] & 0x0F));
+
+                return GenerateGuidFromBytes(bytes);
+            }
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Uuid.Test.Unit/Versions/UuidTest.V4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Cayd.Uuid.Test.Unit
+{
+    public partial class UuidTest
+    {
+        [Fact]
+        public void V4_Generate_ShouldGenerateUniqueGuids()
+        {
+            // Arrange
+            var guids = new List<Guid>();
+
+            // Act
+            for (int i = 0; i < _numberOfGuidsToGenerate; ++i)
+            {
+                guids.Add(Uuid.V4.Generate());
+            }
+
+            // Assert
+            var guidSet = guids.ToHashSet();
+            Assert.Equal(guids.Count, guidSet.Count);
+        }
+    }
+}
