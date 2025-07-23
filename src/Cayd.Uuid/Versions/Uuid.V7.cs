b7f32283e0b70ef48291619cb0473fb0e8b54a74
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-23 14:44:41 +0200

    add uuid v7 generator

---
+++ src/Cayd.Uuid/Versions/Uuid.V7.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Cayd.Uuid
+{
+    public static partial class Uuid
+    {
+        public static class V7
+        {
+            public static Guid Generate()
+                => GenerateGuid(DateTimeOffset.UtcNow);
+
+            public static Guid Generate(DateTimeOffset offset)
+                => GenerateGuid(offset);
+
+            private static Guid GenerateGuid(DateTimeOffset offset)
+            {
+                var bytes = new byte[16];
+
+#if NETSTANDARD2_0
+                using (var rng = RandomNumberGenerator.Create())
+                {
+                    rng.GetBytes(bytes);
+                }
+#else
+                RandomNumberGenerator.Fill(bytes);
+#endif
+
+                long timestamp = offset.ToUnixTimeMilliseconds();
+
+                // 'unix_ts_ms' bits
+                bytes[0] = (byte)((timestamp >> 40) & 0xFF);
+                bytes[1] = (byte)((timestamp >> 32) & 0xFF);
+                bytes[2] = (byte)((timestamp >> 24) & 0xFF);
+                bytes[3] = (byte)((timestamp >> 16) & 0xFF);
+                bytes[4] = (byte)((timestamp >> 8) & 0xFF);
+                bytes[5] = (byte)(timestamp & 0xFF);
+
+                // 'ver' bits
+                bytes[6] = (byte)(0x70 | (bytes[6] & 0x0F));
+
+                // 'var' bits
+                bytes[8] = (byte)(0x80 | (bytes[8] & 0x3F));
+
+                return GenerateGuidFromBytes(bytes);
+            }
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Uuid.Test.Unit/Versions/UuidTest.V7.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System;
+using Xunit;
+using System.Linq;
+
+namespace Cayd.Uuid.Test.Unit
+{
+    public partial class UuidTest
+    {
+        [Fact]
+        public void V7_Generate_ShouldGenerateUniqueGuids()
+        {
+            // Arrange
+            var guids = new List<Guid>();
+
+            // Act
+            for (int i = 0; i < _numberOfGuidsToGenerate; ++i)
+            {
+                guids.Add(Uuid.V7.Generate());
+            }
+
+            // Assert
+            var guidSet = guids.ToHashSet();
+            Assert.Equal(guids.Count, guidSet.Count);
+        }
+    }
+}
